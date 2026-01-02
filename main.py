fbd4f04b787991777c72e5934dab22f7bbd31d6e
etshre <estherr.w54@gmail.com>
2026-01-02 19:23:39 +0000

    preproccessed the information
    makes it easier for the bot to take in the data and takes out small day to day fluctuations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
+from env import StockTradingEnv
 
 def load_stock_data(ticker_symbol, start_date, end_date):
     
@@ -13,3 +14,30 @@ def load_stock_data(ticker_symbol, start_date, end_date):
     else: 
         print("could not return data")
         return None
+    
+    df = df.drop(columns=['Dividends', 'Stock Splits'], errors='ignore')
+
+
+TICKER = 'NVDA'
+START_DATE = '2000-01-01'
+END_DATE = '2026-01-01'
+
+stock_df = load_stock_data(TICKER, START_DATE, END_DATE)
+
+if stock_df is None:
+    print(stock_df.head())
+
+def preprocess_data(df):
+    df['Close_Norm'] = df['Close'] / df['Close'].max()
+    df['Volume_Norm'] = df['Volume'] / df['Volume'].max()
+
+    df['SMA50'] = df['Close'].rolling(window=50).mean()
+    df.fillna(method='bfill', inplace = True)
+
+    features_df = df[['Close_Norm', 'Volume_Norm', 'SMA50']]
+    return features_df
+
+if stock_df is not None:
+    processed_df = preprocess_data(stock_df)
+    print('\nProccessed Data with new features:')
+    print(processed_df.head())
